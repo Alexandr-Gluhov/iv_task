dfa9ce988a55b8da669e1204ec095744462e8372
Alexandr Gluhov <sanjok2505@gmail.com>
2024-11-17 13:52:47 +0300

    User authorization check changed to method

--- templates/auth_bock.php
+++ templates/auth_bock.php
@@ -2,7 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/iv_task/.core/index.php');
 ?>
 <div>
-    <?php if (isset($_SESSION['USER_ID'])) : ?>
+    <?php if (UserLogic::isAuthorized()) : ?>
         <div>Вы вошли как <?=UserTable::get_by_id($_SESSION['USER_ID'])['email']?> <a class="link-dark" href="/iv_task/authentication/logout.php">Выход</a></div>
     <?php else : ?>
         <div>Вы не авторизованы</div>
